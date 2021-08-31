250612e624282d0936394739f530f3ea3832018d
AngelBzDev <angelbaez.dev@gmail.com>
2021-08-31 00:17:50 -0500

    add buttons percentage and enable button reset

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
 //Validate input empty
 
+const form = document.querySelector('form');
+
 const inputBill = document.querySelector('#bill')
 const inputNumPeople = document.querySelector('#number-people')
+const inputCustom = document.querySelector('#custom')
 
 window.onload = () => {
+   initApp()
+}
+
+const initApp = () => {
+   form.addEventListener('submit', (event) => {
+      event.preventDefault()
+   })
    inputBill.addEventListener('input', validateInput)   
    inputNumPeople.addEventListener('input', validateInput)   
+   generateButtons()
+   activeButton()
+   resetInputs()
 }
 
 const validateInput = (e) => {
@@ -29,4 +42,44 @@ const validateInput = (e) => {
       e.target.parentElement.classList.add('input')
       e.target.parentElement.classList.remove('input-err')
    }
+}
+
+const generateButtons = () => {
+   const percentages = [5, 10, 15, 25, 50]
+   const percentageContainer = document.querySelector('.percentage-container')
+   percentages.forEach(i => {
+      const butt = document.createElement('button')
+      butt.textContent = `${i}%`
+      butt.classList.add('btn', 'btn-percentage')
+      percentageContainer.appendChild(butt)
+   })
+   let customInp = document.createElement('input')
+   customInp.id = 'custom'
+   customInp.type = 'number'
+   customInp.classList.add('btn', 'btn-custom')
+   customInp.placeholder = 'Custom'
+   percentageContainer.appendChild(customInp)
+}
+
+const activeButton = () => {
+   const buttonsPorcentages = document.querySelectorAll('.btn-percentage')
+   buttonsPorcentages.forEach(button => {
+      button.addEventListener('click', () => {
+         btnActive = document.querySelectorAll('.btn-active')
+         if(btnActive.length < 1){
+            button.classList.add('btn-active')
+         }
+         else{
+            btnActive[0].classList.remove('btn-active')
+            button.classList.add('btn-active')
+         }
+      })
+   })
+}
+
+const resetInputs = () => {
+   document.querySelector('input[type=reset]').addEventListener('click', () => {
+      const btnActive = document.querySelector('.btn-active')
+      btnActive && btnActive.classList.remove('btn-active')
+   })
 }
